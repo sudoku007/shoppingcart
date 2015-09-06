f1ee140da1114802f8893048b599ddbfb9863d83
sudoku007 <sudoku007@gmail.com>
2015-09-06 20:01:00 +0100

    Added code and test to implement checkout price with simple offers on Apples, Oranges

--- src/main/scala/uk/gov/hmrc/shoppingcart/ShoppingCart.scala
+++ src/main/scala/uk/gov/hmrc/shoppingcart/ShoppingCart.scala
@@ -1,5 +1,7 @@
 package uk.gov.hmrc.shoppingcart
 
+import scala.math.BigDecimal
+
 
 trait Item
 
@@ -7,10 +9,32 @@ case object Apple extends Item
 
 case object Orange extends Item
 
+trait Offer {
+  def applyOffer(items: List[Item]): Int
+}
+
+object AppleBogofOffer extends Offer {
+  def applyOffer(items: List[Item]): Int = items.size / 2 + items.size % 2
+}
+
+object Orange3For2Offer extends Offer {
+  def applyOffer(items: List[Item]): Int = (2 * (items.size / 3)) + items.size % 3
+}
+
+object NoOffer extends Offer {
+  def applyOffer(items: List[Item]): Int = items.size
+}
+
 class ShoppingCart {
 
-  val itemsPrice: Map[Item, BigDecimal] = Map(Apple -> 0.60, Orange -> 0.25)
+  private val itemsPrice: Map[Item, BigDecimal] = Map(Apple -> 0.60, Orange -> 0.25)
+  private val itemsOffer: Map[Item, Offer] = Map(Apple -> AppleBogofOffer, Orange -> Orange3For2Offer)
+  itemsOffer.withDefaultValue(NoOffer)
 
-  def checkoutCost(items: List[Item]): BigDecimal = items.map(itemsPrice).sum
+  def checkoutCost(items: List[Item]): BigDecimal = {
+    items.groupBy(identity)
+      .map({ case (item, listOfItems) => itemsPrice(item) * itemsOffer(item).applyOffer(listOfItems) })
+      .sum
+  }
 
 }
